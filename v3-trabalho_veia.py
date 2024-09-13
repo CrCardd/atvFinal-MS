fa5f466ea4bd34144d9efacec122137e0ebd99eb
CrCardd <cris.rc377@gmail.com>
2024-09-13 17:12:07 -0300

    New tictactoe check winner

--- v3-trabalho_veia.py
+++ v3-trabalho_veia.py
@@ -1,15 +1,33 @@
-def check_play(tab, jog):
-    if tab[0][0] == tab[0][1] and tab[0][1] == tab[0][2] and tab[0][2] != '-' or  tab[1][0] == tab[1][1] and tab[1][1] == tab[1][2] and tab[1][2] != '-' or   tab[2][0] == tab[2][1] and tab[2][1] == tab[2][2] and tab[2][2] != '-' or tab[0][0] == tab[1][0] and tab[1][0] == tab[2][0] and tab[2][0] != '-' or tab[0][1] == tab[1][1] and tab[1][1] == tab[2][1] and tab[2][1] != '-' or tab[0][2] == tab[1][2] and tab[1][2] == tab[2][2] and tab[2][2] != '-' or tab[0][0] == tab[1][1] and tab[1][1] == tab[2][2] and tab[2][2]  != '-'or tab[0][2] == tab[1][1] and tab[1][1] == tab[2][0] and tab[2][0] != '-':
-        if jog%2 == 0:
+def check_play(tab, jog):    
+    check = False
+    h = ''
+    v = ''
+    for i in range(3):
+        h = tab[i][0] + tab[i][1] + tab[i][2]
+        v = tab[0][i] + tab[1][i] + tab[2][i]
+        if h == 'XXX' or v == 'XXX':
             print('O JOGADOR 1 VENCEU!')
-        else:
-            print('O JOGADOR 2 VENCEU!')
-        return True
+            check = True
         
-
-
-
-
+        if h == 'OOO' or v == 'OOO':
+            print('O JOGADOR 2 VENCEU!')
+            check = True
+    
+    z = tab[0][0] + tab[1][1] + tab[2][2]
+    y =  tab[0][2] +  tab[1][1] +  tab[2][0]
+    
+    if z == 'XXX' or y == 'XXX':
+        print('O JOGADOR 1 VENCEU!')
+        check = True
+    if z == 'OOO' or y == 'OOO':
+        print('O JOGADOR 2 VENCEU!')
+        check = True
+    
+    return check
+        
+    
+    
+        
 
 
 
