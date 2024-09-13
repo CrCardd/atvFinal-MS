61ff3bfdbbec0dd6b5718f192b472f0890678f20
CrCardd <cris.rc377@gmail.com>
2024-09-13 15:01:55 -0300

    v1-whilepratica

---
+++ v1-whilepratica.py
@@ -0,0 +1,20 @@
+total = 0
+height = 1
+while height > 0:
+    height = float(input('Qual a altura da pessoa em centimetros ( 0 - sair )? \n>> '))
+    gender = input('Qual o sexo da pessoa (M/F)?\n>> ')
+    if gender.upper() == 'M':
+        resp = (72.7 * height)-58
+    elif gender.upper() == 'F':
+        resp = (62.1*height)-44.7
+    else:
+        print('\nInsira um valor válido!')
+        continue
+
+    print(f'Seu peso ideal é: {round(resp,2)}kg')
+    total += 1
+    
+
+print(f'\n\n\n\nO experimento teve {total} participantes.')
+    
+    
